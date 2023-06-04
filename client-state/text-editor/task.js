cdee401a87f057cda21f759d618d0a73a97ee38b
Allex <inta.dn@yandex.ru>
2023-06-04 15:51:30 +0300

    Внесены правки в реализации текстового редактора

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -2,15 +2,9 @@ const editor = document.getElementById('editor');
 
 window.addEventListener('load', () => {
 	const savedValue = localStorage.getItem('editorValue');
-
-	if(savedValue) {
-		editor.value = savedValue;
-	}
+	editor.value = savedValu;
 });
 
-
 editor.addEventListener('input', () => {
-	const value = editor.value;
-
-	localStorage.setItem('editorValue', value);
+	localStorage.setItem('editorValue', editor.value);
 });
